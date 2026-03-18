e19e7dcbcbd2b8c2a9c40882508b680227f4a0ff
Ricardo Lopez <63953854+LuisRicardoLopezVillafan@users.noreply.github.com>
2026-03-18 03:17:06 -0600

    Create queue.h

---
+++ queue.h
@@ -0,0 +1,33 @@
+/* Una implementación de cola mediante una lista ligada */
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define    EMPTY    0
+#define    FULL     10000
+
+typedef    unsigned int         data;
+typedef    enum {false, true}   boolean;
+
+struct elem {          // Un elemento en la cola.
+    data         d;
+    struct elem  *next;
+};
+
+typedef    struct elem           elem;
+struc queue {
+    int         cnt;                            // Conteo de los elementos de la cola.
+    elem        *head;                          // Apuntador al 'head' de la cola.
+    elem        *tail;                          // Apuntador al 'tail' de la cola.
+};
+
+typedef     struct queue          queue;        // Definimos el tipo de dato cola (queue, se pronuncia "quiu").
+
+/* Operaciones básicas de colas */
+void        initialize(queue *q);               // Inicializar una cola.
+void        enqueue(data d, queue *q);          // Encolar (agregar) un elemento (por tail).
+data        deque(queue *q);                    // Desencolar (quitar) un elemento (por head). Lo que se devuelve es el elemento apuntado por 'head'.
+data        head(const queue *q);               // ¿Cuál es el elemento al inicio de la cola?
+data        tail(const queue *q);               // ¿Cuál es el elemento al final de la cola?
+boolean     empty(const queue *q);              // ¿La cola está vacía?
+boolean     full(const queue *q);               // ¿La cola está llena (10000 elementos)?
